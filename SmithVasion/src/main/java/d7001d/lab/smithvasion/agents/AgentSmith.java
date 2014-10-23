0ecf4eb9536dafc3083c32ecaed3584bc1a1f7db
leo jpod <leo.jpod@gmail.com>
2014-10-23 17:16:55 +0200

    Basic Agent smith completed: Agent smith is merely an agent with 3 params: INetAddress, port and attack period. When running agent smith will simply open a TCP connection toward the given address every attack period and send a silly message. Nothing more.

--- SmithVasion/src/main/java/d7001d/lab/smithvasion/agents/AgentSmith.java
+++ SmithVasion/src/main/java/d7001d/lab/smithvasion/agents/AgentSmith.java
@@ -7,7 +7,10 @@ package d7001d.lab.smithvasion.agents;
 
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
-import java.net.URL;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -17,28 +20,53 @@ import java.util.logging.Logger;
 public class AgentSmith extends Agent{
   private static final Logger logger = Logger.getLogger(AgentSmith.class.getName());
   private long attackPeriod;
-  private URL ip;
+  private int targetPort;
+  private InetAddress targetAddress;
   
   private void parseParams(Object[] args) {
-    
+    if (args.length > 3) {
+      try {
+        targetAddress = (InetAddress) args[0];
+      } catch (ClassCastException ex) {
+        logger.log(Level.SEVERE, "Wrong target address parameter!", ex);
+        targetAddress = null;
+        return;
+      }
+      try {
+        targetPort = (int) args[1];
+      } catch (ClassCastException ex) {
+        logger.log(Level.INFO, "Wrong target port parameter", ex);
+      }
+      try {
+        attackPeriod = (long) args[2];
+      } catch (ClassCastException ex) {
+        logger.log(Level.INFO, "Wrong attack period parameter", ex);
+      }
+    }
   }
   
   @Override
   protected void setup() {
     Object[] args = this.getArguments();
     parseParams(args);
+    //"final" parameters for now
+    this.addBehaviour(new AttackBehavior(this, attackPeriod));
   }
   
   private final class AttackBehavior extends TickerBehaviour{
-
     public AttackBehavior(Agent a, long period) {
       super(a, period);
     }
-
     @Override
     protected void onTick() {
-      //TODO Not supported yet
-      throw new UnsupportedOperationException("Not supported yet.");
+      try {
+        Socket s = new Socket(targetAddress, targetPort);
+        s.getOutputStream().write("42\r\n".getBytes());
+        s.getOutputStream().write("Welcome in the matrix Neo\r\n".getBytes());
+        s.getOutputStream().flush();
+      } catch (IOException ex) {
+        logger.log(Level.SEVERE, null, ex);
+      }
     }
   }
   

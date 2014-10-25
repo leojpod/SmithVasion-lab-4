cc9d4d06bcc26df97c69a9827a114a454930ee18
leo jpod <leo.jpod@gmail.com>
2014-10-25 18:44:44 +0200

    :start: squeleton for SubCoordAgent [Start #81420528]

---
+++ SmithVasion/src/main/java/d7001d/lab/smithvasion/agents/SubCoordAgent.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package d7001d.lab.smithvasion.agents;
+
+import jade.core.Agent;
+
+/**
+ *
+ * @author leojpod
+ */
+public class SubCoordAgent extends Agent{
+
+  @Override
+  protected void setup() {
+    // register to DF
+    // create cycling behaviour
+    // end
+  }
+  
+  
+}

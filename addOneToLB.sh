60963cdb381a9c0da14208f1f81a28eaffb75740
Titotix <titotix@free.fr>
2014-10-27 15:06:53 +0000

    new instance + update lb config

---
+++ addOneToLB.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Launch a new aws instance from $AMI image
+AMI=ami-1205ac65
+#number of new instances
+NUMBER=1
+debug=/home/ubuntu/debug
+aws_output_creation=/home/ubuntu/aws_output_creation
+aws_output=/home/ubuntu/aws_output
+su root -c "aws ec2 run-instances --image-id $AMI --count $NUMBER --instance-type t2.micro --key-name 14\ _LP1_KEY_\ D7001D_thimar-4 --subnet-id subnet-47ca2d1e > $aws_output_creation"
+line=$(head -n 2 $aws_output_creation)
+echo $line > $aws_output
+instance=$(cut -d" " -f 10 $aws_output)
+echo $instance > $debug
+
+# add the previous instance to the load balancer
+su root -c "aws elb register-instances-with-load-balancer --instances $instance --load-balancer-name thimarLB2"
--- launchNewInstance.sh
+++ launchNewInstance.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+# This script launch a new aws isntance from an image I pre configure
+# The output of this script is th enew instance ID
 
 if [ "$#" -ne 1 ]; then
 	echo "Usage : number of instance to launch in parameter"
 	exit 1
 fi
-AMI=ami-6803aa1f
+AMI=ami-1205ac65
 NUMBER=$1
 aws ec2 run-instances --image-id $AMI --count $NUMBER --instance-type t2.micro --key-name 14\ _LP1_KEY_\ D7001D_thimar-4 --subnet-id subnet-47ca2d1e > aws_output
 line=$(head -n 2 aws_output)

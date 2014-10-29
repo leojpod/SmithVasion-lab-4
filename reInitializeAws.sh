f666876776f6ba5c7b356aef15aeb7cd789a7bcc
Titotix <titotix@free.fr>
2014-10-29 23:29:20 +0000

    Best commit ever

--- reInitializeAws.sh
+++ reInitializeAws.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -x
 
 instList=`su root -c "aws elb describe-load-balancers --load-balancer-names thimarLB2 | grep INSTANCES | cut -f2 | sed ':a;N;$!ba;s/\n/ /g'"`
+#dont delete base
 instList=`echo $instList | sed -e 's/i-f41f2db6//g' `
+# dont delete jade server
 instList=`echo $instList | sed -e 's/i-d65e4894//g' `
 instList=`echo $instList | sed -e 's/i-9c7066de//g' `
 su root -c "aws elb deregister-instances-from-load-balancer --load-balancer-name thimarLB2 --instances $instList"

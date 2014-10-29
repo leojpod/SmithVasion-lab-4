2bfa063c5a761907c52c4a019091307947ad4268
Titotix <titotix@free.fr>
2014-10-29 18:46:57 +0000

    add powerfull script

---
+++ reInitializeAws.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x
+
+instList=`su root -c "aws elb describe-load-balancers --load-balancer-names thimarLB2 | grep INSTANCES | cut -f2 | sed ':a;N;$!ba;s/\n/ /g'"`
+instList=`echo $instList | sed -e 's/i-f41f2db6//g' `
+su root -c "aws elb deregister-instances-from-load-balancer --load-balancer-name thimarLB2 --instances $instList"
+su root -c "aws ec2 terminate-instances --instance-ids $instList"

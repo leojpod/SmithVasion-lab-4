b32000ff98cc1c59f205d2192ff7a3bf7b7d0c1e
Titotix <titotix@free.fr>
2014-10-29 23:43:11 +0000

    script to launch new subCoord server

---
+++ newSubCoord.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+AMI=ami-580fa72f
+aws_output_creation=/home/ubuntu/aws_output_creation
+aws_output=/home/ubuntu/aws_output
+su root -c "aws ec2 run-instances --image-id $AMI --count 1 --instance-type t2.micro --key-name 14\ _LP1_KEY_\ D7001D_thimar-4 --subnet-id subnet-47ca2d1e > $aws_output_creation"
+line=$(head -n 2 $aws_output_creation)
+echo $line > $aws_output
+instance=$(cut -d" " -f 10 $aws_output)
+echo $instance
+rm aws_output_creation aws_output
